57d89ac5c31c197f8cd530669974fddc100f0620
Rodrigo <romartinez1997@yahoo.com.ar>
2025-08-04 15:10:51 -0300

    Agregadas todas las funcionalidades requeridas, con algunas extras

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var arrayAmigos = [];
+const regexSoloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+
+document.getElementById('button-reset').disabled = true;
+
+function actualizarBotonReset() {
+    buttonReset = document.getElementById('button-reset');
+    if (arrayAmigos.length === 0) {
+        buttonReset.disabled = true;
+    } else {
+        buttonReset.disabled = false;
+    }
+}
+
+function agregarAmigo(nombreAmigo) {
+    var nombreAmigo = document.getElementById('amigo').value.trim();
+
+    if (nombreAmigo == '') {
+        alert('El nombre no puede estar vacío.');
+    } else if (!regexSoloLetras.test(nombreAmigo)) {
+        alert('Por favor, ingrese un nombre válido.');
+    } else {
+        arrayAmigos.push(nombreAmigo);
+        document.getElementById('amigo').value = '';
+        listarAmigos();
+        actualizarBotonReset();
+    }
+}
+
+function listarAmigos() {
+    var lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (var i = 0; i < arrayAmigos.length; i++) {
+        var li = document.createElement('li');
+        li.textContent = arrayAmigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (arrayAmigos.length == 0) {
+        alert('No hay amigos en la lista, no se puede realizar el sorteo.');
+    } else {
+        var indiceRandom = Math.floor(Math.random() * arrayAmigos.length);
+        var amigoRandom = arrayAmigos[indiceRandom];
+        document.getElementById('resultado').innerHTML = `El amigo seleccionado es: ${amigoRandom}`;
+    }
+}
+
+function limpiarLista() {
+    arrayAmigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
+    document.getElementById('amigo').value = '';
+    actualizarBotonReset();
+}
+
